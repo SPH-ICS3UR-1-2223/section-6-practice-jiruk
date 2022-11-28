993ff281e6af843495eec12dd4ad301d54b46591
jnievehz <jimwel.nieves@stu.ocsb.ca>
2022-11-28 14:08:31 -0500

    uncomplete

---
+++ LoopShape/LoopShape.java
@@ -0,0 +1,16 @@
+public class LoopShape {
+    
+    static void createRectangle(int width, int height){
+        //Draw a Rectangle
+    system.out.println(); 
+    	
+    }
+    
+    
+    
+    static void createTriangle(int leg){
+        //Draw an Isosceles Right Triangle
+        
+        
+    }
+}
