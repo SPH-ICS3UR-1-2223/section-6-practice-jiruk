67d1a1fd6e427af6f61d5fba7db591443898d44b
jimmy nieves <jimwel.nieves@stu.ocsb.ca>
2022-11-28 21:15:43 -0500

    final touch

--- src/ValidatePin/ValidatePin.java
+++ src/ValidatePin/ValidatePin.java
@@ -6,20 +6,20 @@ public class ValidatePin {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int pin = 4200;
+        int pin = 0200;
         System.out.println("Enter a PIN");
         int userPin=in.nextInt();
-        int userGuesses=3;
+        int userGuesses=5;
         while (userPin!=pin) {
         	System.out.println("invalid PIN. Try again.");
-        	if (userGuesses>3)  {
+        	if (userGuesses>5)  {
         		break;
         	}
         	userGuesses++;
         	userPin=in.nextInt();
         }
         if (userPin!=pin) {
-        	System.out.println("You ran out of guesses. Police have been notified");
+        	System.out.println("You ran out of guesses. A pin change request has been sent to your email");
         }else {
         	System.out.println("PIN correct. Access granted");
         }
